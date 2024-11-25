0eb814dd93eefad8db7fe52f98d7682f94bda883
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2024-11-25 13:03:01 +0100

    Add initial Hashnotes application

---
+++ app.py
@@ -0,0 +1,77 @@
+import base64
+import hashlib
+import markdown
+
+from flask import (
+    abort,
+    Flask,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
+from pathlib import Path
+from werkzeug.security import safe_join
+
+app = Flask(__name__)
+
+def ensure_dir_exists(path):
+    path.mkdir(parents=True, exist_ok=True)
+
+def get_digest(content):
+    digest = hashlib.sha256(content.encode("utf-8")).digest()
+    return base64.urlsafe_b64encode(digest).decode("ascii")
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+# TODO: Add upper limit for text size
+@app.route("/edit/", methods=["GET", "POST"], defaults={"filename": ""})
+@app.route("/edit/<filename>", methods=["GET", "POST"])
+def edit(filename):
+    content = ""
+    if request.method == "POST":
+        content = request.form["content"]
+        digest = get_digest(content)
+        with open(f"notes/{digest}", "w") as f:
+            f.write(content)
+        return redirect(url_for("edit", filename=digest))
+    content = ""
+    file_path = safe_join("notes", filename)
+    if not file_path:
+        abort(404)
+    if filename:
+        with open(file_path, "r") as f:
+            content = f.read()
+    return render_template("edit.html", content=content)
+
+# TODO: Cache rendered Markdown somehow (add Nginx file cache?)
+# TODO: Support deleting content by adding a "blocklist"
+# TODO: Add cache headers (expiry time can theoretically be set to infinity,
+# but this would not be compatible with the idea of "deleting" content)
+@app.route("/<filename>", methods=["GET"])
+def view(filename):
+    file_path = safe_join("notes", filename)
+    if not file_path:
+        abort(404)
+    with open(file_path, "r") as f:
+        content = markdown.markdown(
+            f.read(),
+            extensions=[
+                "fenced_code",
+                "codehilite",
+                "smarty",
+                "toc",
+                "footnotes",
+        ])
+    return render_template(
+        "view.html", filename=filename, content=content)
+
+
+# @app.route("/static/<path:path>")
+# def static(path):
+#     return send_from_directory("static", path)
+
+# NOTE: Ensure that dir exists on startup
+ensure_dir_exists(Path("notes"))
